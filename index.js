e0a8f678f15b4942138613bd8aea78052d437703
Gautam Halvadiya <halvadiyagautam57@gmail.com>
2026-04-09 23:54:36 +0530

    some add

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 let full = document.getElementById("full");
 let password = document.getElementById("password");
 let email = document.getElementById("email");
+let dates = document.getElementById("date")
+let photo = document.getElementById("photo")
 
-// document.querySelectorAll(".form-control").forEach(input => {
-//     input.addEventListener("keyup", function() {
-//         validateSingleField(this);
-//     });
-// });
+document.querySelectorAll(".form-control").forEach(input => {
+    input.addEventListener("keyup", function() {
+        validateSingleField(this);
+    });
+});
 
 document.getElementById("sb-button").addEventListener("click", validation);
 
 let regex = /^[a-zA-Z]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
 let regPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%&!])[A-Za-z\d@#$%&!]{8,}$/;
-let regName = /^[a-zA-Z]{3,}$/;
+let regName = /^\S[a-zA-Z, .'-]{2,}$/;
 
-// function validateSingleField(element) {
+function validateSingleField(element) {
 
-//     validateField(element, regName, "Name must be at least 3 letters");
+    if(element.id == "full")
+        validateField(element, regName, "At least 3 letters; no starting space.");
+    else if(element.id == "email")
+        validateField(element, regex, "Enter valid email");
+    else if(element.id == "password")
+        validateField(element, regPass, "Password must be strong");
+    else if(element.id == "date")
+        validateField(element, /^/,"DOB is mandatory");
+    else if(element.id == "photo")
+        validateField(element, /^/,"Please attach photo");
+    else if(element.id == "address")
+        validateField(element, /^.[a-zA-Z0-9,.-]{10,}$/,"Please write valid Address");
     
-// }
+}
 
 
 function validation() {
 
-    validateField(full, regName, "Name must be at least 3 letters");
+    validateField(full, regName, "At least 3 letters; no starting space.");
     validateField(email, regex, "Enter valid email");
     validateField(password, regPass, "Password must be strong");
-
-    
+    validateField(dates, /^/,"DOB is mandatory");
+    validateField(photo, /^/,"Please attach photo");
+    validateField(address, /^.[a-zA-Z0-9,.-]{10,}$/,"Please write valid Address");
 
 }
 
